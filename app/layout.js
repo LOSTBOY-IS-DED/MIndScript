e68ded536c502267d5e3cdbc28c92f032ecaa98c
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2024-11-10 05:07:27 +0530

    configured with clerk

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Outfit } from "next/font/google";
 import "./globals.css";
-import provider from "./provider";
+import Provider from "./provider";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const outfit = Outfit({ subsets: ["latin"] });
 
@@ -11,11 +12,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={outfit.className}>
-        <Provider>{children}</Provider>
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={outfit.className}>
+          <Provider>{children}</Provider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
