7ec1955126675ed8826570241b2208e99aa77a74
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2024-11-10 04:48:03 +0530

    whole app wrapped inside convex provider

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Outfit } from "next/font/google";
 import "./globals.css";
+import provider from "./provider";
 
 const outfit = Outfit({ subsets: ["latin"] });
 
@@ -11,7 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={outfit.className}>{children}</body>
+      <body className={outfit.className}>
+        <Provider>{children}</Provider>
+        {children}
+      </body>
     </html>
   );
 }
